68843d458f97ad273aaef9c1705c80e15217f89c
solawing <316786359@qq.com>
2015-10-20 17:29:07 +0800

    refacotor project

---
+++ luacocoa/testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by SolaWing on 15/10/19.
+//  Copyright © 2015年 sw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
